2d08ab735d5e605d19422cde62f0e788bd57e716
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-28 09:51:57 +0300

    the example does not work

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Read Me First
 
-The original idea is from  
-https://medium.com/@furkanalniak/spring-boot-actuator-effortless-app-monitoring-2a6ecf7470e
-
+Some endpoints are  
 http://localhost:8080/actuator/health
 http://localhost:8080/actuator/metrics
 http://localhost:8080/actuator/env
--- src/main/java/com/colak/springactuatormicrometertutorial/config/SecurityConfig.java
+++ src/main/java/com/colak/springactuatormicrometertutorial/config/SecurityConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
+    // When spring security is enabled only health endpoint is accessible by default
+    // The configuration below does not work
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
