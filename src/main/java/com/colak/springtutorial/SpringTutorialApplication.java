e77157d606cdc29894135d2019482960b416d027
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-18 11:09:23 +0300

    upgrade spring

--- src/main/java/com/colak/springtutorial/SpringTutorialApplication.java
+++ src/main/java/com/colak/springtutorial/SpringTutorialApplication.java
@@ -1,13 +1,13 @@
-package com.colak.springactuatormicrometertutorial;
+package com.colak.springtutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringActuatorMicrometerTutorialApplication {
+public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringActuatorMicrometerTutorialApplication.class, args);
+		SpringApplication.run(SpringTutorialApplication.class, args);
 	}
 
 }
